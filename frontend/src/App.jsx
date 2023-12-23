2a79845734909e758ebc782e4f1c9bb51997eab7
Satyam Gupta <satya.supercluster@gmail.com>
2023-12-23 19:58:30 +0530

    first commit project structured

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="text-3xl bg-black font-bold underline">
+      <h1>
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
