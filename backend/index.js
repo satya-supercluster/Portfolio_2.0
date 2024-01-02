6db0c1e3897a3dc98ffd126e940b2f301bcb994f
Satyam Gupta <satya.supercluster@gmail.com>
2024-01-02 21:56:53 +0530

    backend setup for nodemailer

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express =require('express');
+const app=express();
+
+//Cors
+const cors=require('cors');
+app.use(cors());
+
+require('dotenv').config();
+
+// LOGGING ALL THE METHODS PERFORMED
+const morgan=require('morgan');
+app.use(morgan('common'));
+
+//MIDDLEWARES for getting json data as request
+const bodyparser=require('body-parser');
+app.use(express.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+// Route
+const mailer = require('./routes');
+
+// Listening the server
+app.listen(process.env.PORT,(err)=>{
+    if(err){
+        console.log("An error occured while listening");
+    }
+    else{
+        console.log(`Here I listened something at port ${process.env.PORT}`);
+    }
+})
