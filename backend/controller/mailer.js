4400147004a27490bbaa9dc7c0e24d40d58fe3bc
Satyam Gupta <satya.supercluster@gmail.com>
2024-01-04 18:26:51 +0530

    stmp

--- backend/controller/mailer.js
+++ backend/controller/mailer.js
@@ -1,42 +1,43 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
-async function sendMail(){
-}
+async function sendMail() {}
 
-const mailer = async (req,res)=>{
-    const {name,email,message}=req.body;
-    const transporter = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-            user: process.env.SEMAIL,
-            pass: process.env.PASSWORD
-        }
-    })
-    const mailDetails = {
-        from: process.env.SEMAIL,
-        to: email,
-        subject: 'Responce From Satyam',
-        text: `Hey ${name.trim().split(' ')[0]} \n Thanks For Shooting your valuable message to me. I'll reply you as soon as possible.`
-    };
-    try{
-        transporter.sendMail(mailDetails);
-    }
-    catch{
-        res.status(500).send({success:false});
-    }
-    const mailDetailsForMe = {
-        from:  process.env.SEMAIL,
-        to: process.env.EMAIL,
-        subject: `${name.trim().split(' ')[0]} Shoot a message From Portfolio`,
-        text: `Name: ${name} \n Email: ${email} \n Message: ${message}`
-    };
-    try{
-        transporter.sendMail(mailDetailsForMe);
-    }
-    catch{
-        res.status(500).send({success:false});
-    }
-    res.status(200).send({success:true});
-}
+const mailer = async (req, res) => {
+  const { name, email, message } = req.body;
+  const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: process.env.SEMAIL,
+      pass: process.env.PASSWORD,
+    },
+  });
+  const mailDetails = {
+    from: process.env.SEMAIL,
+    to: email,
+    subject: "Responce From Satyam",
+    text: `Hey ${
+      name.trim().split(" ")[0]
+    } \n Thanks For Shooting your valuable message to me. I'll reply you as soon as possible.`,
+  };
+  try {
+    transporter.sendMail(mailDetails);
+  } catch {
+    res.status(500).send({ success: false });
+  }
+  const mailDetailsForMe = {
+    from: process.env.SEMAIL,
+    to: process.env.EMAIL,
+    subject: `${name.trim().split(" ")[0]} Shoot a message From Portfolio`,
+    text: `Name: ${name} \n Email: ${email} \n Message: ${message}`,
+  };
+  try {
+    transporter.sendMail(mailDetailsForMe);
+  } catch {
+    res.status(500).send({ success: false });
+  }
+  res.status(200).send({ success: true });
+};
 
 module.exports = mailer;
